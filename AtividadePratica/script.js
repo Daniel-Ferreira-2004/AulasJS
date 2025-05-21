8e5773e529095931483a66b0e0d8a4487826a66f
Daniel-Ferreira-2004 <danikferreira69@gmail.com>
2025-05-20 21:48:21 -0300

    Troca de imagem em JS

---
+++ AtividadePratica/script.js
@@ -0,0 +1,12 @@
+function changeImage() {
+    const img = document.getElementById("myImage");//Aqui conseguimos pegar o elemento do HTML, o img//
+    const currentSrc = img.getAttribute("src"); //Aqui pegamos o atributo SRC da imagem//
+
+    if (currentSrc === "tigre.jpg") {
+        img.setAttribute("src", "img/vavalo.jpg"); //Se a origem atual da imagem foi tigre.jpg, pegue o atributo src e mude para vavalo.jpg//
+        img.setAttribute("alt", "Imagem de um Vavalo");
+    } else {
+        img.setAttribute("src", "img/tigre.jpg");
+        img.setAttribute("alt", "Imagem de um tigre");
+    }
+}
